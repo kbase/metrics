e8809d4fdad0b0dcc801852bc43aa9d3f42bfa2a
Shane Canon <scanon@lbl.gov>
2015-02-01 18:22:51 -0800

    For merge_narratives - Addiontal stats - More metadata and pretty print

--- scripts/merge_narrative.pl
+++ scripts/merge_narrative.pl
@@ -13,15 +13,32 @@ use Data::Dumper;               # Perl core module
 use warnings;                   # Good practice
 
 my $outdir=$ARGV[0];
+my $staff_file="kbase-staff.lst";
+my $merged_file="narratives2.json";
 
 my $json = JSON->new->allow_nonref;
 
+# Read in the Staff list
+#
+my %staff;
+open(E,$staff_file) or die "Unable to open KBase Staff list";
+while(<E>){
+  chomp;
+  $staff{$_}=1;
+}
+close E;
+
+# From access logs
 my $accessurl = "https://narrative.kbase.us/access_log/access.json";
+
+# From workspace
 my $objurl = "http://metrics.kbase.us/ws_object_list.json";
 
 my $jsontmp = get( $accessurl );
 die "Could not get $accessurl!" unless defined $jsontmp;
 
+# Write out the access data unaltered
+#
 open(O,"> $outdir/narrative_access.json") or die "Unable to open output ($outdir)";
 print O $jsontmp;
 close O;
@@ -33,8 +50,11 @@ $jsontmp = get( $objurl );
 die "Could not get $objurl!" unless defined $jsontmp;
 
 # Decode the entire JSON
+my $by_month;
 my $objlist = JSON::decode_json( $jsontmp );
 
+# Go through the access logs and augment the workspace data
+#
 for my $obj (keys %{$access->{'by_workspace'}}){
   next unless defined $objlist->{$obj};
   $objlist->{$obj}->{access_count}=$access->{'by_workspace'}->{$obj}->{access_count};
@@ -47,6 +67,12 @@ for my $obj (keys %{$access->{'by_workspace'}}){
   }
 }
 
+# Cleanup and counters
+# - Set access to 0 if no access records
+# - Remove objects that lack a name
+# - Remove objects that are deleted the workspace
+# - Remove autosave naratives
+ 
 for my $obj (keys %{$objlist}){
   if (! defined $objlist->{$obj}->{access_count}){
     $objlist->{$obj}->{access_count}=0;
@@ -64,6 +90,46 @@ for my $obj (keys %{$objlist}){
   delete $objlist->{$obj} if $objlist->{$obj}->{name}=~/^auto[0-9]+/;
 }
 
-open(O,"> $outdir/narratives.json") or die "Unable to open output ($outdir)";
-print O $json->pretty->encode( $objlist );
+# Update counters for new narratives
+#
+for my $obj (keys %{$objlist}){
+  my $month=substr($objlist->{$obj}->{first_access},0,7);
+  $by_month->{$month}->{'new_narrative'}++;
+  my $o=$objlist->{$obj}->{savedby};
+  my $s=':user';
+  $s=':staff' if defined $staff{$o};
+  $by_month->{$month}->{'new_narrative'.$s}++;
+}
+
+
+# Generate cumulative values
+#
+my $l='new_narrative';
+my %cum;
+for my $t ('',':staff',':user'){ $cum{$l.$t}=0; }
+for my $month (sort keys %{$by_month}){
+    $by_month->{$month}->{access_count}=$access->{'by_month'}->{$month}->{access_count};
+    for my $t ('',':staff',':user'){
+       my $v=$by_month->{$month}->{$l.$t};
+       $cum{$l.$t}+=$by_month->{$month}->{$l.$t} if defined $v;
+       $by_month->{$month}->{'cumulative_narrative'.$t}=$cum{$l.$t};
+    }
+}
+
+# Assemble object for JSON output
+#
+my $jout->{'by_month'}=$by_month;
+$jout->{'by_workspace'}=$objlist;
+$jout->{'by_date'}=$access->{'by_date'};
+
+# Create metadata block
+my $date=strftime('%Y-%m-%d',gmtime);
+$jout->{meta}->{comments}="Generated from narrative access logs and workspace objects";
+$jout->{meta}->{author}="Shane Canon";
+$jout->{meta}->{generated}=$date;
+$jout->{meta}->{dataset}= "nginx access logs and workspace objects";
+$jout->{meta}->{comments}="This merges the narrative access logs from nginx with a dump of the narrative objects in the workspace.\n";
+
+open(O,"> $outdir/$merged_file") or die "Unable to open output ($outdir)";
+print O $json->pretty->canonical->encode( $jout );
 close O;
