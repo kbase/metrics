aadcbe709c730e9a1c71899ed9a7ee0bd684fc7f
Cheyenne  Nelson <cnelson@docker01.berkeley.kbase.us>
2019-09-13 10:32:50 -0700

    Upload of Elasticsearch methods and upload code to git.

---
+++ source/upload_elasticsearch_usersmry_stats.py
@@ -0,0 +1,7 @@
+
+# UploadElasticsearchStats                                                                                                                                               
+import methods_upload_elasticsearch_sumrydicts.py
+# start_date = "day-month-year"                                                                                                                                       
+# end_date = "day-month-year"  
+
+methods_upload_elasticsearch_sumrydicts.elastic_summary_dictionaries()
