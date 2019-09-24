70be424ebfdfc4e1a6718942450cf9dfe984e6eb
Cheyenne  Nelson <cnelson@login1.berkeley.kbase.us>
2019-09-24 11:01:38 -0700

    Added timing information to functions.

--- source/upload_elasticsearch_usersmry_stats.py
+++ source/upload_elasticsearch_usersmry_stats.py
@@ -4,7 +4,8 @@ import warnings
 import pprint
 warnings.simplefilter(action='ignore', category=Warning)            
 import methods_upload_elasticsearch_sumrydicts
-# start_date = "month-day-year"                                                                                                                              
+# start_date = "month-day-year"                                                                                                                       
 # end_date = "month-day-year"  
+#return_capture = methods_upload_elasticsearch_sumrydicts.elastic_summary_dictionaries(start_date, end_date)
 return_capture = methods_upload_elasticsearch_sumrydicts.elastic_summary_dictionaries()
 pprint.pprint(return_capture)
