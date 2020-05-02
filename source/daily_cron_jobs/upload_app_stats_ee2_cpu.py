e258e1c7a34b40d8d3b6c25f89129838cdd4210e
Jason Baumohl <jkbaumohl@lbl.gov>
2020-05-02 00:55:39 +0000

    added code and table definitions for Apps_usage_ee2 with reserved_cpu

---
+++ source/daily_cron_jobs/upload_app_stats_ee2_cpu.py
@@ -0,0 +1,15 @@
+# UploadAppStats
+#
+import methods_upload_app_stats_ee2_cpu
+import time
+import datetime
+
+print("############################################")
+print("App Stats Upload (UTC): " + str(datetime.datetime.utcnow()))
+start_time = time.time()
+start_date = "2012-02-01"
+# start_date = "2019-08-15"
+end_date = "2020-05-20"
+methods_upload_app_stats_ee2_cpu.upload_user_app_stats(start_date, end_date)
+# methods_upload_app_stats_ee2_cpu.upload_user_app_stats()
+print("Uploading app stats took ", time.time() - start_time, " seconds to run")
