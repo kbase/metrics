9d0e5ceb8a71a2166e428891ce8d107d8e00f136
Jason Baumohl <jkbaumohl@lbl.gov>
2021-02-18 00:12:32 +0000

    combined object changes views

--- sql_create_statements/sql_reporting_views_and_tables.sql
+++ sql_create_statements/sql_reporting_views_and_tables.sql
@@ -1211,22 +1211,14 @@ create or replace view metrics_reporting.users_object_changes as
 select object_type, 
 DATE_FORMAT(`record_date`,'%Y-%m') as record_month,
 sum(total_object_count) as total_object_count, 
-round(sum(total_size)/1000000000,4) as total_size_GB
-from metrics.users_workspace_object_counts
-group by object_type, record_month
-order by object_type, record_month;
-
-
-#--------------------------------------------------
-# USER TOP LVL workspace objects changes over time
-#--------------------------------------------------
-create or replace view metrics_reporting.users_top_lvl_object_changes as
-select object_type, 
-DATE_FORMAT(`record_date`,'%Y-%m') as record_month,
+round(sum(total_size)/1000000000,4) as total_size_GB,
 sum(top_lvl_object_count) as top_lvl_object_count, 
 round(sum(top_lvl_size)/1000000000,4) as top_lvl_size_GB
 from metrics.users_workspace_object_counts
 group by object_type, record_month
 order by object_type, record_month;
+
+
+
 
 
