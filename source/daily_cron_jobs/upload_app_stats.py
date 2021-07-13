e893b64e3491b80487d3818f50d4dfc0881133e1
Jason Baumohl <jkbaumohl@lbl.gov>
2021-07-13 19:00:48 +0000

    fixed minor screwup with commented out start time for logging

--- source/daily_cron_jobs/upload_app_stats.py
+++ source/daily_cron_jobs/upload_app_stats.py
@@ -6,7 +6,7 @@ import datetime
 
 print("############################################")
 print("App Stats Upload (UTC): " + str(datetime.datetime.utcnow()))
-#start_time = time.time()
+start_time = time.time()
 #start_date = "2021-06-01"
 #end_date = "2022-07-20"
 #methods_upload_app_stats.upload_user_app_stats(start_date, end_date)
