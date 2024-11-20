d11c2303b9729e8d3e1e1fd4583149607eb1195f
Jason Baumohl <jkbaumohl@lbl.gov>
2024-11-20 23:48:28 +0000

    Metrics cron machine memory upped, hopefully it handle blobstore details now

--- bin/master_cron_shell.sh
+++ bin/master_cron_shell.sh
@@ -14,7 +14,7 @@ python daily_cron_jobs/upload_public_narratives_count.py
 
 python daily_cron_jobs/upload_user_orcid_count.py
 
-#python daily_cron_jobs/upload_blobstore_details.py
+python daily_cron_jobs/upload_blobstore_details.py
 
 python daily_cron_jobs/make_reporting_tables.py
 
