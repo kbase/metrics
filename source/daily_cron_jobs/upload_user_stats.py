806c25000e490e926ccf7053b45194953eba6b68
crockettz <62550319+crockettz@users.noreply.github.com>
2026-07-08 12:10:55 -0400

    Modify get_profile_info call with new parameters

--- source/daily_cron_jobs/upload_user_stats.py
+++ source/daily_cron_jobs/upload_user_stats.py
@@ -13,7 +13,7 @@ user_stats_dict = methods_upload_user_stats.get_user_orgs_count(user_stats_dict)
 user_stats_dict = methods_upload_user_stats.get_user_narrative_stats(user_stats_dict)
 user_stats_dict = methods_upload_user_stats.get_user_orcidlinks(user_stats_dict)
 #user_stats_dict = methods_upload_user_stats.get_institution_and_country(user_stats_dict)
-user_stats_dict = methods_upload_user_stats.get_profile_info(user_stats_dict)
+user_stats_dict = methods_upload_user_stats.get_profile_info(user_stats_dict,batch_size=100,max_retries=1)
 print("--- gather data %s seconds ---" % (time.time() - start_time))
 methods_upload_user_stats.upload_user_data(user_stats_dict)
 print(
